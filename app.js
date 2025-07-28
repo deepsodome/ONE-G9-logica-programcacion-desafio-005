f5e9e1fcb2c47ab771d35bf8b3466189ea0a5fa5
Raúl Trueba <120868378+deepsodome@users.noreply.github.com>
2025-07-28 10:38:55 -0600

    actualización app.js
    se dieron límites si el usuario ingresa número fuera de rangos
    se corrige número secreto debe estar entre rango de números a adivinar seleccionado por usuario

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 //Variables
 let numeroRangoIni = parseInt(prompt("Hola Amig@! \nApartir de cuál número quieres adivinar?"));   // asigna desde prompt usuario el número de rango
 let numeroRangoFin = parseInt(prompt("Hola Amig@! \nHasta que número quieres adivinar?"));   // asigna desde prompt usuario el número de rango
-let numeroSecreto = Math.floor(Math.random(numeroRangoIni)*numeroRangoFin)+1; // crea variable Y asigna el número a encontrar
+let numeroSecreto = Math.floor(Math.random() * (numeroRangoFin - numeroRangoIni + 1)) + numeroRangoIni;
+ // crea variable Y asigna el número a encontrar
 let numeroUsuario = 0; // crea variable asignada para input de usuario igual a 0
 //let textVeces = 'intento';  //crea variable texto para mensaje y se asigna en singular
 let maximoIntentos = 0; //crea variable y se asigna el máximo
@@ -26,9 +27,9 @@ while(numeroSecreto != numeroUsuario){  // repite hasta el maximo numero de inte
     /*
         compara valores de variables y despliega mensaje si la edad es mayot a diez
     */
-   if (numeroUsuario < numeroRangoIni || numeroUsuario > numeroRangoFin){       //compara compara que el numero de usuario sea mayor a rango inicial y menor al rango final
-        alert(`¡Número inválido! \nEl rango es entre ${numeroRangoIni} y ${numeroRangoFin} \nFIN DEL JUEGO \nDa clic en -Aceptar- y pulsa la tecla -F5- para intentarlo de nuevo.`);        //muestra error numero usuario es menor a rango inicial y mayor a rango final
-        break;      //sale de loop
+   if (numeroUsuario < numeroRangoIni || numeroUsuario > numeroRangoFin){       //compara compara que el numero de usuario sea mayor a rango inicial y menor al rango final        
+    alert(`¡Número inválido! \nEl rango para adivinar es entre ${numeroRangoIni} y ${numeroRangoFin} \nTe quedan ${(maximoIntentos - intentos)}  ${(maximoIntentos - intentos) == 1 ?'intento' : 'intentos'}`);        //muestra error numero usuario es menor a rango inicial y mayor a rango final
+    //intentos++; //incrementa intentos
     }
     if (numeroSecreto == numeroUsuario) {       //compara si numero secreto es igual a numero usuario
         alert(`CORRECTO! \nEl número es: ${numeroUsuario}. Lo lograste en ${intentos} ${intentos == 1 ? 'intento' : 'intentos'}`);      // lanza mensaje de aciertó
@@ -36,16 +37,16 @@ while(numeroSecreto != numeroUsuario){  // repite hasta el maximo numero de inte
     else { // no se cumple concidión
         //alert("No adivinaste el número!"); // mensaje de no acierta
         if (numeroUsuario < numeroSecreto){
-            alert("ERROR! \nEl número es mayor");       //lanza mensaje avisando si el número es mayor
+            alert("AYUDA! \nEl número es mayor");       //lanza mensaje avisando si el número es mayor
         } else {
-            alert("ERROR! \nEl número es menor");       //lanza mensaje avisando si el número es menor
+            alert("AYUDA! \nEl número es menor");       //lanza mensaje avisando si el número es menor
         }
         //incrementar el contador
         //intentos +=1;
         intentos++;
         //textVeces = 'intentos';       //asigna valor si fueron más de un intento
         if (intentos > maximoIntentos){  //compara cuantos intentos se han hecho
-            alert(`AVISO! \nLlegaste al número máximo de intentos: ${maximoIntentos} \nDa clic en -Aceptar- y pulsa la tecla -F5- para intentarlo de nuevo.`);    //lanza mensaje avisando número de intentos
+            alert(`PERDISTE! \nLlegaste al número máximo de intentos: ${maximoIntentos} \nDa clic en -Aceptar- y pulsa la tecla -F5- para intentarlo de nuevo.`);    //lanza mensaje avisando número de intentos
             break;      //sale de loop
         }
     }
